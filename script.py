e3d5c1284fbd4081ccea9918e72ae086eac98ff3
MeVikashhh <vikash.test@outlook.com>
2024-10-10 15:40:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import boto3
 import traceback
 
 # Configuration
