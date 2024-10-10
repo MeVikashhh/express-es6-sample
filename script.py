56996e25a9d76fbecd314627ecfa035c013bab5f
MeVikashhh <vikash.test@outlook.com>
2024-10-10 15:06:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ project_name = 'node_build_app'
 region_name = 'us-east-1'
 ses_sender_email = 'vikash.indoqubix@gmail.com' 
 recipient_email = 'vikash.kumar@indoqubix.com'  # Replace with the recipient's email
-smtp_username = 'vikash.kumar@indoqubix.com'  # Your Gmail address
+smtp_username = 'vikash.indoqubix@gmail.com'  # Your Gmail address
 smtp_password = 'rxhs asuc ygnt rdqv'  # Your Gmail app password
 
 def get_latest_build_id(project_name):
